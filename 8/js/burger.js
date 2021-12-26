8875a2649df847f487bfa819466ef0867b5e1ac4
Keks <keks@htmlacademy.ru>
2021-12-27 01:42:09 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/burger.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.toggle');
+let nav = document.querySelector('.navigation');
+burger.addEventListener('click', function(event) {
+  burger.classList.toggle('toggle--activ');
+  nav.classList.toggle('navigation--close');
+});
+nav.classList.remove('navigation--nojs');
