8d6f56611ae3b4c847bd4003b347a47edc5ee59b
Vladimir <mesvladimir@yandex.ru>
2022-01-12 22:11:29 +0300

    Правки по замечаниям

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,7 +1,9 @@
 let burger = document.querySelector('.toggle');
 let nav = document.querySelector('.navigation');
+let map = document.querySelector('.map');
 burger.addEventListener('click', function(event) {
   burger.classList.toggle('toggle--activ');
   nav.classList.toggle('navigation--close');
 });
 nav.classList.remove('navigation--nojs');
+map.classList.remove('map--nojs');
