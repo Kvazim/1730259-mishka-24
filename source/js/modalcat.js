59729828ed3a197ab18b49bd888e56c0b7eb6092
Vladimir <mesvladimir@yandex.ru>
2022-01-22 01:16:06 +0300

    Правки по замечаниям

--- source/js/modalcat.js
+++ source/js/modalcat.js
@@ -1,17 +1,28 @@
-let bybtn = document.querySelectorAll('.card__button');
+let btncard = document.querySelectorAll('.card__button');
 let modalClose = document.querySelector('.modal');
 let modalCont = document.querySelector('.modal-container');
-bybtn.forEach(function(bybtn){
-    bybtn.addEventListener('click', function(event) {modalClose.classList.remove('modal--close');
+
+btncard.forEach(function(btncard) {
+
+    btncard.addEventListener('click', function(event) {
+        modalClose.classList.remove('modal--close');
     });
 });
+
 //закрытие окна по escape
-window.addEventListener("keydown", function (e) {
+window.addEventListener('keydown', function (e) {
+
     if (e.which == 27) {
         modalClose.classList.add('modal--close');
     };
 });
+
 //закрытие окна по клику на область
-modalClose.addEventListener('click', function() {
-    modalClose.classList.add('modal--close');
+const someAction = () => modalClose.classList.add('modal--close');
+
+window.addEventListener('click', (e) => {
+
+    if (e.target !== modalCont && !modalCont.contains(e.target)) {
+        someAction();
+    }
 });
